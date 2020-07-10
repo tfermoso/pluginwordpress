e22cc90319bf5d68a6f6d1f5adbe9abd1b7f3e8f
Tomas <tomas.fermoso@gmail.com>
2020-07-10 16:32:25 +0200

    paso 1 creacion plugin

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Formulario Autoevaluacion
+Plugin URI: https://autoevaluacion.com
+Description: Formulario autoevaluación
+Version: 1.0
+Author: Tomás
+Author URI: www.tomasfermoso.com
+License: GPLv2 or later
+Text Domain: formulario
+ */
+
+ 
